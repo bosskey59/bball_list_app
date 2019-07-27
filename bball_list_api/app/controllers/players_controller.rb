d283bcd0063c33baff6a2a169171c5b62ff7ee06
Alejandro Aguilar Jr <a.a.j@live.com>
2019-07-27 18:38:15 -0500

    added ruby create for player

--- bball_list_api/app/controllers/players_controller.rb
+++ bball_list_api/app/controllers/players_controller.rb
@@ -1,6 +1,14 @@
 class PlayersController < ApplicationController
     # take in list_id and name from front end.
     def create
-
+        @location = Location.find_by_id(params[:locationId])
+        @last_list = @location.lists.last
+        if @last_list.players.length < 5
+            @player = Player.create(list: @last_list, name:params[:name])
+        else
+            @list = List.create(location: @location)
+            @player = Player.create(list: @list, name:params[:name])
+        end
+        render json: @player
     end
 end
